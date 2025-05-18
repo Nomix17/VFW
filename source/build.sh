e5c3476f1af740e31c2b5189846a8cf864ae534f
Nomix <145512477+Nomix17@users.noreply.github.com>
2025-05-18 17:34:10 +0000

    Add files via upload

---
+++ source/build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e 
+
+BASE_NAME=$(basename *.pro .pro)
+
+if [ ! -d "../bin" ]; then
+  mkdir ../bin
+fi
+
+cd ../bin
+
+qmake ../source/*.pro
+make clean
+make
+
+# ./"$BASE_NAME"
+
+mkdir -p /opt/VFW
+cp -r ../cache /opt/VFW/
+cp -r ../bin /opt/VFW/
+
+cp ../cache/run.sh /usr/bin/VFW
+
+cp ../cache/VFW.desktop ~/.local/share/applications
+
