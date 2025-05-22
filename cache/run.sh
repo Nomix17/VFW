c2eea58df4011926bfa852455db021e73505b2c9
Nomix <145512477+Nomix17@users.noreply.github.com>
2025-05-22 17:13:42 +0000

    Update run.sh

--- cache/run.sh
+++ cache/run.sh
@@ -1,9 +1,23 @@
-dirpath=$(pwd)
-mediapath="$dirpath/$1"
-cd "/opt/VFW/bin/"
-if [ "$1" != "" ]; then
-	./VFW "$mediapath"
-else
-	./VFW
-fi
+while [[ $# -gt 0 ]]; do 
+  case "$1" in
+    --fullpath | -p) 
+      shift
+      mediapath="$1" 
+      ;;
+   --nameonly | -n)
+      shift
+      mediapath="$(pwd)/$1"
+      ;;
 
+    *)
+      echo "Unknown option: $1" 
+      exit 1
+      ;;
+
+  esac
+  shift
+
+done
+echo "$mediapath"
+cd "/opt/VFW/bin/"
+./VFW "$mediapath"
