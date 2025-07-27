72fab00ddc329ab125a57a68ca4b466244eec4ec
izayrit <145512477+Nomix17@users.noreply.github.com>
2025-07-27 12:42:00 +0000

    Update run.sh

--- cache/run.sh
+++ cache/run.sh
@@ -1,41 +1,26 @@
 #!/bin/bash
 
-mediapaths=()
-path_mode="fullpath"
+set -e
 
-while [[ $# -gt 0 ]]; do 
-  case "$1" in
-    --fullpath | -p) 
-      path_mode="fullpath"
-      ;;
-    --nameonly | -n)
-      path_mode="nameonly"
-      ;;
-    -*)
-      echo "Unknown option: $1" 
-      exit 1
-      ;;
-    *)
+BASE_NAME=$(basename *.pro .pro)
 
-      if [[ "$path_mode" == "fullpath" ]]; then
-        mediapaths+=("$1")
-      else
-        mediapaths+=("$(pwd)/$1")
-      fi
-      ;;
-  esac
-  shift
-done
-
-if [[ ${#mediapaths[@]} -eq 0 ]]; then
-  echo "No files specified"
-  exit 1
+if [ ! -d "../bin" ]; then
+  mkdir ../bin
 fi
 
-echo "Processing files:"
-for path in "${mediapaths[@]}"; do
-  echo "  $path"
-done
+cd ../bin
 
-cd "/opt/VFW/bin/"
-./VFW "${mediapaths[@]}"
+qmake6 ../source/*.pro
+make clean
+make
+
+# ./"$BASE_NAME"
+
+mkdir -p /opt/VFW
+cp -r ../cache /opt/VFW/
+cp -r ../bin /opt/VFW/
+cp ../source/uninstall.sh /opt/VFW/bin
+
+cp ../cache/run.sh /usr/bin/VFW
+
+cp ../cache/VFW.desktop $1/.local/share/applications
