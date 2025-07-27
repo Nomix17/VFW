5dff6a2b9e92c4f9e8670ad2a9f5173d7df6b71d
izayrit <145512477+Nomix17@users.noreply.github.com>
2025-07-27 02:49:41 +0000

    Update run.sh

--- cache/run.sh
+++ cache/run.sh
@@ -1,24 +1,41 @@
 #!/bin/bash
+
+mediapaths=()
+path_mode="fullpath"
+
 while [[ $# -gt 0 ]]; do 
   case "$1" in
     --fullpath | -p) 
-      shift
-      mediapath="$1" 
+      path_mode="fullpath"
       ;;
-   --nameonly | -n)
-      shift
-      mediapath="$(pwd)/$1"
+    --nameonly | -n)
+      path_mode="nameonly"
       ;;
-
-    *)
+    -*)
       echo "Unknown option: $1" 
       exit 1
       ;;
+    *)
 
+      if [[ "$path_mode" == "fullpath" ]]; then
+        mediapaths+=("$1")
+      else
+        mediapaths+=("$(pwd)/$1")
+      fi
+      ;;
   esac
   shift
-
 done
-echo "$mediapath"
+
+if [[ ${#mediapaths[@]} -eq 0 ]]; then
+  echo "No files specified"
+  exit 1
+fi
+
+echo "Processing files:"
+for path in "${mediapaths[@]}"; do
+  echo "  $path"
+done
+
 cd "/opt/VFW/bin/"
-./VFW "$mediapath"
+./VFW "${mediapaths[@]}"
