17f54f3a39d8c8d9f29f9bbbdcc939f7e9ffe12a
Nomix17 <izayrit12345678@gmail.com>
2026-02-02 13:12:07 +0100

    oops

--- source/uninstall.sh
+++ source/uninstall.sh
@@ -2,6 +2,6 @@ ASSETS_DIR="$HOME/.local/share/VFW"
 CONFIGS_DIR="$HOME/.config/VFW"
 CACHE_DIR="$HOME/.cache/VFW"
 BIN_DIR="/usr/local/bin/VFW"
-DESKTOP_ENTRY="$HOME/.local/share/applications/"
+DESKTOP_ENTRY="$HOME/.local/share/applications/VFW.desktop"
 
 sudo rm -rf $ASSETS_DIR $CONFIGS_DIR $BIN_DIR $DESKTOP_ENTRY
