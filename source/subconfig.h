f99991158c8cf801dfc5e892f2e2bd0f484fc86b
Nomix17 <izayrit12345678@gmail.com>
2026-02-03 19:08:16 +0100

    adding option to customize subtitles font weight

--- source/subconfig.h
+++ source/subconfig.h
@@ -39,7 +39,8 @@ private:
   std::vector <QString>labels={"Margin Bottom:","Padding:",
                       "Bg Color:","Bg Opacity:",
                       "Font Size:","Font Familly:",
-                      "Font Color:","Font Opacity:"
+                      "Font Color:","Font Opacity:",
+                      "Font Weight:"
   };
   QVBoxLayout * mainlayout;
   QGridLayout * gridlayout;
@@ -158,6 +159,8 @@ public:
     settings["font-familly"]=selected_font.toStdString();
 
     settings["font-color"]=this->findChild<QPushButton*>("FontColor")->palette().color(QPalette::Button).name().toStdString();
+    settings["font-weight"]=std::stoi(this->findChild<QLineEdit*>("Font Weight:")->text().toStdString());
+
     settings["font-opacity"]=std::stoi(this->findChild<QLineEdit*>("Font Opacity:")->text().toStdString());
 
     std::ofstream file(fullSubConfigPath);
@@ -183,6 +186,7 @@ public:
      
       this->findChild<QPushButton*>("FontColor")->setStyleSheet("background-color:"+QString::fromStdString(configfile["font-color"])+";");
       this->findChild<QLineEdit*>("Font Opacity:")->setText(QString::number(configfile["font-opacity"].get<int>()));
+      this->findChild<QLineEdit*>("Font Weight:")->setText(QString::number(configfile["font-weight"].get<int>()));
 
       selected_font = QString::fromStdString(configfile["font-familly"]);
 
@@ -207,18 +211,20 @@ public:
 
       QString backgroundColorRGB = QString::number(bgcolor.red()) + ", " + QString::number(bgcolor.green()) + ", " + QString::number(bgcolor.blue());
       QString backgroundColorAlpha = QString::number(static_cast<double>(settings["bg-opacity"]) / 100, 'f', 2);
-      QString textColorRGB = QString::number(fontcolor.red()) + ", " + QString::number(fontcolor.green()) + ", " + QString::number(fontcolor.blue());
-      QString textColorAlpha = QString::number(static_cast<double>(settings["font-opacity"]) / 100, 'f', 2);
+      QString fontColorRGB = QString::number(fontcolor.red()) + ", " + QString::number(fontcolor.green()) + ", " + QString::number(fontcolor.blue());
+      QString fontColorAlpha = QString::number(static_cast<double>(settings["font-opacity"]) / 100, 'f', 2);
+      QString fontWeight = QString::number(static_cast<double>(settings["font-weight"]), 'f', 2);
       QString paddingValue = QString::number(static_cast<double>(settings["padding"]), 'f', 2);
 
-    QString HtmlScript = "<table cellpadding='" + paddingValue + "' "
-                         "style='"
-                         "background-color: rgba(" + backgroundColorRGB + ", " + backgroundColorAlpha + "); "
-                         "color: rgba(" + textColorRGB + ", " + textColorAlpha + "); "
-                         "font-family: \"" + QString::fromStdString(settings["font-familly"]) + "\"; "
-                         "font-size: " + QString::number(static_cast<int>(settings["font-size"])) + "px;"
-                         "'>"
-                         "<tr><td>";
+      QString HtmlScript = 
+        "<table cellpadding='" + paddingValue + "' "
+          "style='"
+            "background-color: rgba(" + backgroundColorRGB + ", " + backgroundColorAlpha + "); "
+            "color: rgba(" + fontColorRGB + ", " + fontColorAlpha + "); "
+            "font-family: \"" + QString::fromStdString(settings["font-familly"]) + "\"; "
+            "font-size: " + QString::number(static_cast<int>(settings["font-size"])) + "px;"
+            "font-weight: " + fontWeight + "'>"
+            "<tr><td>";
 
       padding = settings["padding"];
       marginbottom = settings["margin-bottom"];
@@ -248,6 +254,7 @@ public:
     defaultsettings["font-color"]= "#ffffff";
     defaultsettings["font-familly"]="Noto Sans Arabic";
     defaultsettings["font-opacity"]= 100;
+    defaultsettings["font-weight"]= 300;
     defaultsettings["font-size"]= 24;
     defaultsettings["margin-bottom"]= 61;
     defaultsettings["padding"]= 26;
