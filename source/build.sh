fc6f1821febb524824fa68d3f99dc0ad3c7caabc
Nomix17 <izayrit12345678@gmail.com>
2026-02-06 18:47:53 +0100

    improving Application Main Icon handling

--- source/build.sh
+++ source/build.sh
@@ -5,15 +5,18 @@ set -e
 BASE_NAME=$(basename *.pro .pro)
 ASSETS_DIR="$HOME/.local/share/VFW"
 CONFIGS_DIR="$HOME/.config/VFW"
+DESKTOP_ENTRIES_DIR="$HOME/.local/share/applications/"
+ICONS_DIR="/usr/share/icons/hicolor/256x256/apps/"
 
 createDirs() {
-  mkdir -p ../bin "$ASSETS_DIR" "$CONFIGS_DIR"
+  mkdir -p ../bin "$ASSETS_DIR" "$CONFIGS_DIR" "$ICONS_DIR"
 }
 
 createDirs
 
 cp -r ../assets/. "$ASSETS_DIR"/
 cp -r ../configs/. "$CONFIGS_DIR"/
+sudo cp ../assets/icons/VFW-Squircle-Dark.png "$ICONS_DIR/vfw.png"
 
 cd ../bin
 
@@ -23,4 +26,4 @@ make
 
 sudo cp ./VFW /usr/local/bin
 
-cp ../VFW.desktop "$HOME/.local/share/applications/"
+cp ../VFW.desktop $DESKTOP_ENTRIES_DIR
