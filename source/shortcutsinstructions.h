df548b290d8ab47e09095a4af14f9a2671fc20a9
Nomix17 <izayrit12345678@gmail.com>
2026-02-11 21:21:43 +0100

    cleaning some code

--- source/shortcutsinstructions.h
+++ source/shortcutsinstructions.h
@@ -24,7 +24,7 @@ class ShortcutsInst:public QDialog{
   public:
     ShortcutsInst(QWidget *parent,std::string StyleDirectory,std::string ConfigDirectory):QDialog(parent){
       this->setWindowTitle("Shortcuts");
-   
+
       //load style file
       std::filesystem::path styleFullPath(std::filesystem::path(StyleDirectory) / "shortcutsinstructions.css");
       std::ifstream stylefile(styleFullPath);
@@ -42,57 +42,51 @@ class ShortcutsInst:public QDialog{
       std::string shortcutsfilecontent;
       std::vector <std::string> elements;
 
-
-
       //checking if the file exist
-      if(shortcutfile){
+      if (shortcutfile) {
         std::string line;
         //we read the file line by line and add the configs into the vector 
         while(getline(shortcutfile,line)){
           elements.push_back(line);
         }
-      }else{
+
+      } else {
         //if it's not found we create a new one 
         std::ofstream defaultshortcutsfile(fullShortcutFilename.string());
 
         //put the default configs into the vector
-        elements={"Functionality:Shortcut",
-                  "FullScreen/Unfullscreen:F",
-                  "Unfullscreen:Escape",
-                  "Pause/Unpause:Space",
-                  "Go Back:LeftArowkey",
-                  "Go Forward:RightArowkey",
-                  "Mute/Unmute:M",
-                  "Volume Up:UpArrowKey",
-                  "Volume Down:DownArrowKey",
-                  "Reduce Video Delay:G",
-                  "Increase Video Delay:H"};
-              
+        elements={
+            "Functionality:Shortcut",
+            "FullScreen/Unfullscreen:F",
+            "Unfullscreen:Escape",
+            "Pause/Unpause:Space",
+            "Go Back:LeftArowkey",
+            "Go Forward:RightArowkey",
+            "Mute/Unmute:M",
+            "Volume Up:UpArrowKey",
+            "Volume Down:DownArrowKey",
+            "Reduce Video Delay:G",
+            "Increase Video Delay:H"
+        };
+
         //pushing the default config in the file
-        for(size_t i=0;i<elements.size();i++){
-          defaultshortcutsfile << elements[i]+'\n';
-        }
-        defaultshortcutsfile.close();//closing the file
+        for(size_t i=0;i<elements.size();i++) defaultshortcutsfile << elements[i]+'\n';
+        defaultshortcutsfile.close();
       }
 
       //looping the elements of the vectore to create widgets based on them
       for(size_t i=0;i<elements.size();i++){
-        std::string shortcutfunctionality;
-        std::string shortcutkeys;
-        int counter=0;
-        //looping the line to split it in too, the keys of the shortcut, and the functionality of it
-        while(elements[i][counter]!=':'){
-          shortcutfunctionality +=elements[i][counter];
-          counter++;
-        }
-          counter++;
-        while(elements[i][counter]!='\0'){
-          shortcutkeys +=elements[i][counter];
-          counter++;
-        }
-        QLabel *functionalitylabel = new QLabel(QString::fromStdString(shortcutfunctionality));
-        QLabel *keyslabel = new QLabel(QString::fromStdString(shortcutkeys));
-        if(i==0){
+        std::stringstream line(elements[i]);
+        std::string shortcutFunctionality;
+        std::string shortcutKeys;
+
+        std::getline(line, shortcutFunctionality,':');
+        std::getline(line, shortcutKeys,':');
+
+        QLabel *functionalitylabel = new QLabel(QString::fromStdString(shortcutFunctionality));
+        QLabel *keyslabel = new QLabel(QString::fromStdString(shortcutKeys));
+
+        if (i==0) {
           functionalitylabel->setObjectName("tablehead");
           keyslabel->setObjectName("tablehead");
         }
