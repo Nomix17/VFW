eb6edccdef4083563cb002de030e326ea98b8f0f
Nomix17 <izayrit12345678@gmail.com>
2026-07-21 02:59:04 +0100

    fix: suppress video context menu when right-clicking over the floating control panel

--- source/mainwindow/UICreation.cpp
+++ source/mainwindow/UICreation.cpp
@@ -77,6 +77,7 @@ void MainWindow::setupVideoDropConnector() {
 
 void MainWindow::setupContextMenuConnector() {
   connect(video, &VideoItemContainer::openContextMenu, this, [this](QPointF pos) {
+    if(floatingControlPannel->isHovered(pos.toPoint())) return;
     ContextMenu* menu = new ContextMenu(this);
     connect(menu, &ContextMenu::handleButtonsClick, this, &MainWindow::onToolMenuAction);
     connect(menu, &QMenu::aboutToHide, [this]() { contextMenuOpened = false; });
